7ea5bb2e68dcf1daf6f1253a93af300b76eb6258
anirbandas <anirbandas_ju@yahoo.co.in>
2013-09-23 18:19:34 +0530

    intital commit for hello world client

---
+++ src/com/git/demo/GitProjClass.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+public class GitProjClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Demo Git Project");
+	}
+
+}
